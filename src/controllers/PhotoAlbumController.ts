d1260062890f8a1f228d629b27354367e9605c0b
Steve Galili <steve@meetfeli.com>
2018-12-03 23:20:49 +0100

    cleanup

--- src/controllers/PhotoAlbumController.ts
+++ src/controllers/PhotoAlbumController.ts
@@ -7,41 +7,17 @@ export default class PhotoAlbumController implements Controller {
         httpServer.get('/albums', this.list);
         httpServer.get('/album/:id', this.getById);
         httpServer.get('/album/:id/pdf', this.generatePdfById);
-        // httpServer.post('/album', this.create.bind(this));
-        // httpServer.put('/album/:id', this.update.bind(this));
-        // httpServer.delete('/album/:id', this.remove.bind(this));
     }
 
     private async list(req: Request, res: Response): Promise<void> {
         console.log("list fired")
-        // res.send(await albumService.list());
     }
 
     private async getById(req: Request, res: Response): Promise<void> {
         console.log("getById fired with id ", req.params.id)
-        // const album = await albumService.getById(req.params.id);
-        // res.send(album ? 200 : 404, album);
     }
 
     private async generatePdfById(req: Request, res: Response): Promise<void> {
         console.log("generatePdfById fired with id ", req.params.id)
     }
-
-    // private async create(req: Request, res: Response): Promise<void> {
-    //     res.send(await albumService.create(req.body));
-    // }
-    //
-    // private async update(req: Request, res: Response): Promise<void> {
-    //     res.send(await albumService.update({...req.body, id: req.params.id}));
-    // }
-    //
-    // private async remove(req: Request, res: Response): Promise<void> {
-    //     try {
-    //         await albumService.delete(req.params.id);
-    //         res.send(200);
-    //     }
-    //     catch (e) {
-    //         res.send(500);
-    //     }
-    // }
 }
