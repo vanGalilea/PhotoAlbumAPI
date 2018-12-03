f785b24451fdd6614b0034b6a0a3b2d68814ef1a
Steve Galili <steve@meetfeli.com>
2018-12-03 22:17:58 +0100

    * tsconfig and loggin addition

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import * as express from 'express';
 import { Application } from 'express';
 import * as bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
+import * as logger from 'morgan';
 
 dotenv.config();
 
 const app: Application = express();
 
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/ping', (req, res)=> {
