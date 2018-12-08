6bc7fd4d945ec85f72bc6269a90e3b5f7234b124
Steve Galili <steve@meetfeli.com>
2018-12-08 12:52:16 +0100

    * create single album route, control mods and service

--- src/controllers/AlbumController.ts
+++ src/controllers/AlbumController.ts
@@ -1,7 +1,6 @@
 import {HttpServer} from '../server/HttpServer';
 import IController from "./IController";
 import {Request, Response} from 'express';
-import DatabaseProvider from "../db";
 import albumService from "../services/AlbumService";
 
 export default class AlbumController implements IController {
@@ -9,7 +8,7 @@ export default class AlbumController implements IController {
         httpServer.get('/albums', this.list);
         httpServer.get('/album/:id', this.getById);
         httpServer.get('/album/:id/html', this.viewAlbum);
-        httpServer.post('/create', this.create);
+        httpServer.post('/album', this.create);
         httpServer.delete('/album/:id', this.remove);
     }
 
@@ -30,51 +29,13 @@ export default class AlbumController implements IController {
 
     private async remove(req: Request, res: Response): Promise<void> {
         const deletedAlbumId = await albumService.remove(req.params.id);
-        res.status(200).send(`Removed album with id ${deletedAlbumId}`);
+        deletedAlbumId > 0 ?
+            res.status(200).send(`Removed album with id ${deletedAlbumId}`) :
+            res.status(404).send('Not Found');
     }
 
     private async create(req: Request, res: Response): Promise<void> {
-        const sequelize = DatabaseProvider.getConnection();
-        const {Album, Page} = DatabaseProvider.getModels();
-
-        sequelize.sync()
-            .then(() => Album.create(
-                {
-                    title: "My album I",
-                    description: "This album is about animals",
-                    Pages: [
-                        {
-                            photosCount: 2,
-                            Photos: [
-                                {url: "https://cdn-kiosk-api.telegraaf.nl/75740900-deb8-11e7-96da-81a0dd833b3d.jpg"},
-                                {url: "http://roflzoo.com/pics/201409/tn_bunny-is-not-impressed.jpg"}
-                            ]
-                        },
-                        {
-                            photosCount: 2,
-                            Photos: [
-                                {url: "https://image.shutterstock.com/image-photo/colorful-photograph-isolated-alpaca-wild-450w-280228817.jpg"},
-                                {url: "https://img.izismile.com/img/img4/20110923/640/these_funny_animals_800_640_01.jpg"}
-                            ]
-
-                        },
-                        {
-                            photosCount: 1,
-                            Photos: [
-                                {url: "http://roflzoo.com/pics/042010/cute-little-bunny-tn.jpg"}
-                            ]
-                        },
-                    ]
-                },
-                {
-                    include: [{
-                        association: Album.Pages,
-                        include: Page.Photos
-                    }] as any
-                }
-            ))
-            .then(album => {
-                res.json(album.toJSON());
-            });
+        const newAlbum = await albumService.create(req.body);
+        res.json(newAlbum.toJSON());
     }
 }
--- src/services/AlbumService.ts
+++ src/services/AlbumService.ts
@@ -1,6 +1,10 @@
-import {modelInstance} from "../types";
-import dbProvider from "../db";
+import {genericAttrs, modelInstance} from "../types";
+import dbProvider, {genericProjectModel} from "../db";
 import {Response} from 'express';
+import * as ORM from 'sequelize';
+import {IAlbumAttrs} from "../models/Album";
+
+//add here you rest CRUD operations implementations
 
 class AlbumService {
     public async list(): Promise<modelInstance[]> {
@@ -32,16 +36,30 @@ class AlbumService {
     }
 
 
-    public async view(albumId: number, res: Response): Promise<modelInstance> {
-        return await this.getById(albumId);
-    }
-
     public async remove(albumId: number): Promise<number> {
         const {Album} = dbProvider.getModels();
         return await Album.destroy({where: {id: albumId}})
     }
 
-    //add here you rest CRUD implementations => create, delete, update
+    public async create(album: IAlbumAttrs): Promise<ORM.Instance<genericAttrs>> {
+        const {Album, Page} = dbProvider.getModels();
+        const sequelize = dbProvider.getConnection();
+        const {title, description, Pages} = album;
+        return sequelize.sync()
+            .then(() => Album.create(
+                {title, description, Pages},
+                {
+                    include: [{
+                        association: Album.Pages,
+                        include: Page.Photos
+                    }] as any
+                }
+            ))
+    }
+
+    public async view(albumId: number, res: Response): Promise<modelInstance> {
+        return await this.getById(albumId);
+    }
 }
 
 export default new AlbumService();
